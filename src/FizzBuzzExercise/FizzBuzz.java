d0813def3234bdf43dff89232727959b7d4f11be
doarcutine <85968252@qq.com>
2017-09-08 14:13:33 +0800

    FizzBuzz Exercise

---
+++ src/FizzBuzzExercise/FizzBuzz.java
@@ -0,0 +1,25 @@
+package FizzBuzzExercise;
+
+/**
+ * Created by Doarcutine on 2017/9/8.
+ */
+public class FizzBuzz {
+    public static void main(String[] args) {
+        final int N = 100;
+
+        for (int i = 1; i <= N; i++) {
+            if (i % 3 != 0 && i % 5 != 0) {
+                System.out.println(i);
+                continue;
+            }
+
+            if (i % 3 == 0) {
+                System.out.print("Fizz");
+            }
+            if (i % 5 == 0) {
+                System.out.print("Buzz");
+            }
+            System.out.println();
+        }
+    }
+}
