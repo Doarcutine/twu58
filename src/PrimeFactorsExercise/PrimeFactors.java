916b14976adc44ac47fa469a70488365dc609b5a
doarcutine <85968252@qq.com>
2017-09-08 17:36:16 +0800

    Prime Factors Exercise

---
+++ src/PrimeFactorsExercise/PrimeFactors.java
@@ -0,0 +1,29 @@
+package PrimeFactorsExercise;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by Doarcutine on 2017/9/8.
+ */
+public class PrimeFactors {
+    public static void main(String[] args) {
+        List<Integer> primeFactors = new ArrayList<Integer>();
+
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                primeFactors.add(i);
+                n /= i;
+                i--;
+            }
+        }
+
+        for (Integer factor : primeFactors) {
+            System.out.print(factor + " ");
+        }
+    }
+}
