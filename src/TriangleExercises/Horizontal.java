e6fcf1ca1e3ed25e768934bb841c7a8e6cbb44b1
doarcutine <85968252@qq.com>
2017-09-08 13:19:54 +0800

    Draw a horizontal line

---
+++ src/TriangleExercises/Horizontal.java
@@ -0,0 +1,14 @@
+package TriangleExercises;
+
+import java.util.Scanner;
+
+
+public class Horizontal {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Integer n = s.nextInt();
+        for(int i =0;i<n;++i) {
+            System.out.print("*");
+        }
+    }
+}
