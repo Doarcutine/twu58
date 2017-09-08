f0dcb8661468ecc880a60e8f0f8d3076dd1285ec
doarcutine <85968252@qq.com>
2017-09-08 14:03:14 +0800

    Diamond

---
+++ src/DiamondExercises/Diamond.java
@@ -0,0 +1,35 @@
+package DiamondExercises;
+
+import java.util.Scanner;
+
+/**
+ * Created by Doarcutine on 2017/9/8.
+ */
+public class Diamond {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        int totalCharacterCount = n * 2 - 1;
+        for(int line = 1;line<=totalCharacterCount;line++) {
+            int space = Math.abs(n - line);
+            int count = totalCharacterCount - (space * 2);
+            PrintSpace(space);
+            PrintAsterisk(count);
+            PrintSpace(space);
+            System.out.println();
+        }
+    }
+
+    private static void PrintSpace(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print(" ");
+        }
+    }
+
+    private static void PrintAsterisk(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+    }
+}
