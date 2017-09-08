b070a7954a78abebc7f3e142e4d7652bcef7b91d
doarcutine <85968252@qq.com>
2017-09-08 13:12:42 +0800

    Initial commit

---
+++ src/TriangleExercises/Easiest.java
@@ -0,0 +1,10 @@
+package TriangleExercises;
+
+/**
+ * Created by Doarcutine on 2017/9/8.
+ */
+public class Easiest {
+    public static void main(String[] args) {
+        System.out.println("*");
+    }
+}
